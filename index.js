ef9764e7197bd64355424b29defe582a6b27acc8
Simran-B <simran@arangodb.com>
2016-04-30 03:07:24 +0200

    Placeholder replacement still does weird things

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ module.exports = {
     html: {
       "body:start": function(current) {
         
-        var basePath = current.basePath === "." ? "" : current.basePath;
-        console.log("basePath:", basePath);
-        console.dir(current.book);
+        var rootPath = current.basePath === "." ? ".." : "../" + current.basePath;
+        console.log("rootPath:", rootPath);
         var filePath = path.join(this.book.root, "HEADER.html");
         
         try {
             var content = fs.readFileSync(filePath, {encoding: "utf-8"});
-            return content.replace(/BASE_PATH/g, basePath);
+            return content.replace(/BASE_PATH/g, rootPath);
         } catch(err) {
             // if there's no file to include, just continue
         }
