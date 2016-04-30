9858ac20b24d98ca17260f07664c51c22e11ba4b
Simran-B <simran@arangodb.com>
2016-04-30 02:12:16 +0200

    Replace placeholder BASE_PATH in header to fix relative links

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+var fs = require("fs");
+var path = require("path");
+
 module.exports = {
   website: {
     assets: ".",
     html: {
-      "body:start": function(current, output) {
-        var fs = require("fs");
-        var path = require("path");
+      "body:start": function(current) {
         
+        var BASE_PATH = current.basePath === "." ? "" : current.basePath;
         var filePath = path.join(this.book.root, "HEADER.html");
         
         try {
-            return fs.readFileSync(filePath);
+            return fs.readFileSync(filePath, {encoding: "utf-8"})
+                replace(/BASE_PATH/g, BASE_PATH);
         } catch(err) {
-            // pass
+            // if there's no file to include, just continue
         }
       }
     }
